03d825b901c90793dafc21ba416d1e2535c8de79
Oussama Lahmami <olahmami@e2r11p6.1337.ma>
2022-11-12 03:50:30 +0100

    ft_printf

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olahmami <olahmami@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 08:05:56 by olahmami          #+#    #+#             */
+/*   Updated: 2022/11/12 02:57:27 by olahmami         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *s);
+void	ft_putstr(char *s);
+
+#endif
